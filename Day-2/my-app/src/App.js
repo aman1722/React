a66c817c841ac747c285a6489a9bcd925a9c9a37
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-10-10 21:21:40 +0530

    Counter

--- Day-2/my-app/src/App.js
+++ Day-2/my-app/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import {Greetings} from './components/Greetings';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
       <h1>Hey</h1>
       <Greetings/>
+      <Counter />
     </div>
   );
 }
