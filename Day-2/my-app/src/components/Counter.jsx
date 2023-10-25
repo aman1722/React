2aff92ef8344de2f0fc1902deb63ef42028e4320
aman1722 <112754413+aman1722@users.noreply.github.com>
2023-10-25 23:00:34 +0530

    Counter App

--- Day-2/my-app/src/components/Counter.jsx
+++ Day-2/my-app/src/components/Counter.jsx
@@ -1,10 +1,14 @@
+import { useState } from "react";
+
 function Counter (){
+    const [value , setvalue] = useState(0);
     return (
+        
         <>
         <h1>Counter</h1>
-        <span>0</span>
-        <button>+</button>
-        <button>-</button>
+        <span>{value}</span>
+        <button onClick={()=>{setvalue(p=>p+1)}}>+</button>
+        <button onClick={()=>{setvalue(p=>p-1)}} >-</button>
         </>
     );
 }
